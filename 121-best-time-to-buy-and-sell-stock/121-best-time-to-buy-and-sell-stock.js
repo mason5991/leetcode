412c568e5b15d2dbf84e382edcbbc219f6f28646
Mason Mung <mason5991@yahoo.com.hk>
2022-07-03 03:09:59 +0800

    add #121

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+// Sliding window
+
+// Time complexity: O(n)
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let profit = 0,
+    p1 = 0;
+  p2 = 1;
+  while (prices[p2] !== undefined) {
+    if (prices[p2] > prices[p1]) {
+      profit = Math.max(profit, prices[p2] - prices[p1]);
+    } else {
+      p1 = p2;
+    }
+    p2++;
+  }
+  return profit;
+};
+
+// Test cases
+// [7,1,5,3,6,4] => 5
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+// [7,6,4,3,1] => 0
+console.log(maxProfit([7, 6, 4, 3, 1]));
+// [2,1,2,1,0,1,2] => 2
+console.log(maxProfit([2, 1, 2, 1, 0, 1, 2]));
+// [3,3,5,0,0,3,1,4] => 4
+console.log(maxProfit([3, 3, 5, 0, 0, 3, 1, 4]));
